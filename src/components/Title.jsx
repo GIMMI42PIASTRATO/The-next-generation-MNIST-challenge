5be338e16d9e688f0a4f5fb8fd5bdd93b32711bb
GIMMI42PIASTRATO <67071988+GIMMI42PIASTRATO@users.noreply.github.com>
2023-09-06 18:42:40 +0200

    Create the input bar for the API link

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,12 +1,13 @@
 import { useContext } from "react";
 import { ThemeContext } from "../App";
 import style from "../style/Title.module.css";
+import { TbLink } from "react-icons/tb";
 
 export default function Title() {
     const themeStateContext = useContext(ThemeContext);
 
     return (
-        <>
+        <div className={style.titleContainer}>
             {themeStateContext.isClickedN1 ? (
                 <div className={style.title}>
                     The <span className={style.highlight}>next-generation</span>{" "}
@@ -29,6 +30,19 @@ export default function Title() {
                     command line.
                 </div>
             )}
-        </>
+            <div className={style.labelContainer}>
+                <div className={style.label}>API link</div>
+                <div className={style.flexContainer}>
+                    <div className={style.inputContainer}>
+                        <TbLink className={style.icon} />
+                        <span style={{ color: "black" }}>|</span>
+                        <input type="text" placeholder="http://apilink" />
+                    </div>
+                    <div className={style.buttonContainer}>
+                        <div>Connect</div>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
