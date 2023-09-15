b0859b1a20984b5ea4cfc90a0cc2a6d872da1f7c
rikvik2006 <75540124+rikvik2006@users.noreply.github.com>
2023-09-15 18:05:24 +0200

    feat: Added base api URL to context

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,18 @@ export const ThemeContext = React.createContext();
 function App() {
     const [isClickedN1, setIsClickedN1] = useState(true);
     const [isClickedN2, setIsClickedN2] = useState(false);
-
+    const [apiLink1, setApiLink1] = useState(null)
+    const [apiLink2, setApiLink2] = useState(null)
+    
     const themeStateContext = {
         isClickedN1,
         setIsClickedN1,
         isClickedN2,
         setIsClickedN2,
+        apiLink1,
+        setApiLink1,
+        apiLink2,
+        setApiLink2
     };
 
     return (
